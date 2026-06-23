61a24e67ae01d58e0ddcb48e0f4fa6bc5aa25898
DroiD16 <garelyshev@gmail.com>
2026-06-23 22:18:30 +0100

    Set actor sheet minimum widths

--- script/sheet/actor/acolyte.js
+++ script/sheet/actor/acolyte.js
@@ -1,6 +1,11 @@
 import { DarkHeresySheet } from "./actor.js";
 
 export class AcolyteSheet extends DarkHeresySheet {
+    static DEFAULT_OPTIONS = {
+        classes: ["acolyte"],
+        position: { width: 815 }
+    };
+
     static PARTS = {
         form: {
             template: "systems/dark-heresy/template/sheet/actor/acolyte.hbs"
